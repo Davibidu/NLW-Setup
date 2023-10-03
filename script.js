b4089664e8c63f84d78061a77c8facb71f4ed936
Davibidu <davibidu2020@gmail.com>
2023-10-02 23:32:26 -0400

    Add new habits

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
 
 const button = document.querySelector('header button')
- button.addEventListener('click', add)
- form.addEventListener("change", save)
+button.addEventListener('click', add)
+form.addEventListener("change", save)
 
- function add() {
-    const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
-    const dayExists = nlwSetup.dayExists(today)
-     
-    if (dayExists) {
-      alert("Dia já incluso")
-      return
-    }
-        
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
 
-    nlwSetup.addDay(today)
- }
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+
+
+  nlwSetup.addDay(today)
+}
 
 function save() {
-    localStorage.setItem("NLWSetup@dev", JSON.stringify(nlwSetup.data))
+  localStorage.setItem("NLWSetup@dev", JSON.stringify(nlwSetup.data))
 }
 
 const data = JSON.parse(localStorage.getItem("NLWSetup@dev")) || {}
